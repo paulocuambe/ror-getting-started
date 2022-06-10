7576b32b07837fab03dc481bdeec6426b8fb5a90
Paulo Cuambe <pamossecuambe@gmail.com>
2022-06-10 12:11:25 +0200

    ruby on rails getting started tutorial

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+end
